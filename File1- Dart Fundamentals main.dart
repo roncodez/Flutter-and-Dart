4b553c4dc94481c12fe6aa640de7da431eabf02e
roncodez <ronitmalhotraofficial@gmail.com>
2023-04-22 15:06:08 +0530

    first commit

---
+++ File1- Dart Fundamentals main.dart
@@ -0,0 +1,72 @@
+import 'dart:core';
+
+// import 'dart:io';
+// class Num {
+//   int num = 10;
+// }
+
+void main() {
+  // Variable declaration
+  // var firstName = 'Ronit'; // type inference
+  // String lastName = 'Malhotra';
+
+  // Input Output
+  // stdout.writeln('What is your name?: ');
+  // String? name = stdin.readLineSync();
+  // stdout.writeln('Helleooo $name'); // String interpolation
+
+  // // Data Types in Dart
+  // String name = 'Ronit';
+  // var age = 20;
+  // int num = 123;
+  // double deci = 123.456;
+  // bool isGiven = true;
+  // dynamic changer = 'Ball';
+  // changer = true;
+
+  // // Strings in Dart
+  //  String name = 'Ronit';
+  //  String name2 = "Ronit";
+  //  String name3 = "Ronit's toys are in the box";
+  //  String name4 = 'Ronit\'s toys are in the cupboard';
+  //  String name5 = r'Escape sequence \n will also be ignored'; // raw string
+  // // Multiline strings - 3 single quotes or 3 double quotes
+  // String hey1 = '''
+  // Hey its me Ronit
+  // ''';
+  // String hey2 = """ Hey its me again,
+  // Hahahahahah
+  // """;
+
+  // // Type Interconversion
+  // // String to int
+  // var one = int.parse('1');
+  // print(one == 23);
+  // // String to double
+  // var onePointOne = double.parse('1.1');
+  // assert(onePointOne == 1.1);
+  // // int to String
+  // String oneAsString = 1.toString();
+  // assert(oneAsString == '1');
+  // // doublt to String
+  // String piAsString = 3.4159.toStringAsFixed(2);
+  // assert(piAsString == '3.14');
+
+  // // Defining constant variables - cannot change value during runtime
+  // const aConstNum = 0; // int constant
+  // const aConstBool = true; // bool constant
+  // const aConstString = 'a constant string'; // string constant
+  // print(aConstNum);
+  // print(aConstNum.runtimeType);
+  // int? num = null;
+  // print(num);
+  // var n = Num();
+  // int number;
+  // number = n?.num ?? 0;
+  // print(number);
+  // /* ===> if n is an object, access its num property and assign, if not then skip the statement execution */
+  // Ternary Operator --- x>100? print('yes'): print('no');
+  // double x = 2.3;
+  // if(x is int)
+  //   print('Integer');
+}
